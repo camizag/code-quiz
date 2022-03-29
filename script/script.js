0d7f212cb1c3d66c696a156d1f32630bca2fe88f
Camila Zagal <camilazags@gmail.com>
2022-03-29 00:30:52 -0600

    added quiz questions and answers

---
+++ script/script.js
@@ -0,0 +1,27 @@
+var quiz = [
+    {
+        question: "This is a question.",
+        options: ["option1", "option2", "option3", "option4"],
+        answer: "option2",
+    },
+    {
+        question: "This is a question 2.",
+        options: ["option1", "option2", "option3", "option4"],
+        answer: "option1",
+    },
+    {
+        question: "This is a question 3.",
+        options: ["option1", "option2", "option3", "option4"],
+        answer: "option4",
+    },
+    {
+        question: "This is a question 4.",
+        options: ["option1", "option2", "option3", "option4"],
+        answer: "option3",
+    },
+    {
+        question: "This is a question 5.",
+        options: ["option1", "option2", "option3", "option4"],
+        answer: "option5",
+    },
+]
